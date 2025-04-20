b8ef4bf75ce2304775f23b2454d5d08463433a6a
Kepei Lei <kepeilei1102@gmail.com>
2025-04-20 09:37:52 +0800

    Class5 is in Class4

--- src/Class4.java
+++ src/Class4.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class Class4 {
     // Class vs. Object
     // Class is an abstraction of a type of objects
     // Objects are instances of a class
+
+    public ArrayList<Double> temperatures;
 
     //access specifier
     //     modifier
@@ -20,5 +24,37 @@ public class Class4 {
     }
     public int product(int x, int y) {
         return x * y;
+    }
+
+
+    public boolean isMinuteFree(int period, int minutes) {
+        return false;
+    }
+
+    public int findFreeBlock(int period, int duration) {
+        int startIndex = 0;
+
+        while (startIndex < 60) {
+            if (isMinuteFree(period, startIndex)) {
+                int freeLength = 0;
+                //The amount of minutes free starting from startIndex
+                int endIndex = startIndex;
+
+                while (endIndex < 60) {
+                    if (isMinuteFree(period, endIndex)) {
+                        freeLength++;
+                        endIndex ++;
+                        if (freeLength == duration)
+                            return startIndex;
+                    } else {
+                        startIndex = endIndex + 1;
+                    }
+                }
+
+            } else {
+                startIndex ++;
+            }
+        }
+        return -1;
     }
 }
